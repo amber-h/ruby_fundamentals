a89f89621a71d8d82b8c149a3adbf53b2efa3ce4
amber-h <amber.houle3@gmail.com>
2013-08-20 23:14:04 -0400

    VersionSort

---
+++ VersionSort.rb
@@ -0,0 +1,72 @@
+class Array
+  def version_sort
+
+    def to_array(s)
+      a = s.scan(/(\d+)([a-z]*)/).flatten 
+      a.each_with_index.map {|e,i| i.even? ? e.to_i : e!="" ? e : i==a.length-1 ? (?a.ord-1).chr : (?z.ord+1).chr}
+    end
+
+    return self.sort do |a,b|
+      am, bm = to_array(a), to_array(b)
+      len = [am.length, bm.length].min
+      cmp = 0
+      am[0,len].zip(bm[0,len]).each {|pair| break if (cmp = (pair.first<=>pair.last)) != 0}
+      cmp
+    end
+  end
+
+
+end
+filenames = [
+  "foo-1.10.2.ext",
+  "foo-1.11.ext",
+  "foo-1.3.ext",
+  "foo-1.50.ext",
+  "foo-1.8.7.ext",
+  "foo-1.9.3.ext",
+  "foo-1.ext",
+  "foo-10.1.ext",
+  "foo-10.ext",
+  "foo-100.ext",
+  "foo-13.ext",
+  "foo-2.0.0.ext",
+  "foo-2.0.1.ext",
+  "foo-2.0.ext",
+  "foo-2.007.ext",
+  "foo-2.01.ext",
+  "foo-2.012b.ext",
+  "foo-2.01a.ext",
+  "foo-2.0a.ext",
+  "foo-2.0b.ext",
+  "foo-2.1.ext",
+  "foo-25.ext",
+  "foo-6.ext",
+]
+version_sorted_filenames = [
+  "foo-1.ext",
+  "foo-1.3.ext",
+  "foo-1.8.7.ext",
+  "foo-1.9.3.ext",
+  "foo-1.10.2.ext",
+  "foo-1.11.ext",
+  "foo-1.50.ext",
+  "foo-2.0.ext",
+  "foo-2.0a.ext",
+  "foo-2.0b.ext",
+  "foo-2.0.0.ext",
+  "foo-2.0.1.ext",
+  "foo-2.01.ext",
+  "foo-2.1.ext",
+  "foo-2.01a.ext",
+  "foo-2.007.ext",
+  "foo-2.012b.ext",
+  "foo-6.ext",
+  "foo-10.ext",
+  "foo-10.1.ext",
+  "foo-13.ext",
+  "foo-25.ext",
+  "foo-100.ext",
+]
+
+sorted = filenames.version_sort
+puts sorted
